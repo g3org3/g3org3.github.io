29c41e8f66aa5efc46da5e4fa247f2554c3b5140
George <7jagjag@gmail.com>
2014-08-09 13:10:18 -0600

    added a "me" page

---
+++ me/assets/js/functions.js
@@ -0,0 +1,54 @@
+$(function() {
+	smoothScrool(300);
+	workBelt();
+	workLoad();
+});
+
+
+function smoothScrool (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+
+function workBelt() {
+
+  $('.thumb-unit').click(function() {
+    $('.work-belt').css('left','-100%');
+    $('.work-container').show();
+  });
+  
+  $('.work-return').click(function() {
+    $('.work-belt').css('left','0%');
+    $('.work-container').hide(800);
+  });
+
+}
+
+
+function  workLoad() {
+  
+  $.ajaxSetup({ cache: true });
+  
+  $('.thumb-unit').click(function() {
+    var $this = $(this),
+        newTitle = $this.find('strong').text(),
+        newfolder = $this.data('folder'),
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = '/work/'+ newfolder +'.html';
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
+  });
+  
+}
+
+
